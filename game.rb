788d6bcf17ed1ae6b6867b35b02eb00795243990
Arthur Granado <agranado@pluga.co>
2017-03-09 21:32:18 -0300

    Why: Simplify start_game method and unify get_human_spot and eval_board
     * Simplify start_game method to call three new methods:
        ** start_by_printing_the_board: That draw the board and ask play to start to play;
        ** play_game: extract the loop (until) to here where the game happens;
        ** finish_game: simple game over message
    
     * Unify get_human_spot and eval_board because basically the both method do the same task. Get position from players and set it to board.
        ** create some test for player_move (get_human_spot) to refactory
    
     * Create some helper methods to let the code cleaner and easy to refactory

--- game.rb
+++ game.rb
@@ -7,12 +7,16 @@ class GameIO
     " #{board[0]} | #{board[1]} | #{board[2]} \n===+===+===\n #{board[3]} | #{board[4]} | #{board[5]} \n===+===+===\n #{board[6]} | #{board[7]} | #{board[8]} \n"
   end
 
-  def ask_human_for_action
+  def show_input_options
     output "Enter [0-8]:"
   end
 
   def game_over_message
     output "Game over"
+  end
+
+  def waiting_for_input
+    input
   end
 
   def output(content)
@@ -25,59 +29,87 @@ class GameIO
 end
 
 class Game
-  attr_reader :board, :game_io
+  attr_reader :board, :game_input_output
 
-  def initialize(game_io=GameIO.new)
+  def initialize(game_input_output=GameIO.new)
     @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     @com = "X" # the computer's marker
     @hum = "O" # the user's marker
-    @game_io = game_io
+    @game_input_output = game_input_output
   end
 
   def start_game
-    # start by printing the board
-    game_io.draw_board(@board)
-    game_io.ask_human_for_action
+    start_by_printing_the_board(board)
 
-    # loop through until the game was won or tied
-    until game_is_over(@board) || tie(@board)
-      get_human_spot(game_io.input)
-      if !game_is_over(@board) && !tie(@board)
-        eval_board
-      end
-      game_io.draw_board(@board)
-    end
+    play_game(board)
     
-    game_io.game_over_message
+    finish_game
   end
 
-  def get_human_spot(choise=nil)
+  def start_by_printing_the_board(board)
+    draw_board(board)
+    show_input_options
+  end
+
+  def finish_game
+    show_game_over_message
+  end
+
+  def play_game(board)
+    until did_game_finish?(board)
+      player_move(human_choise, @hum)
+      player_move(computer_choise(board), @com) if keep_playing?
+      draw_board(board)
+    end
+  end
+
+  def did_game_finish?(board)
+    game_is_over(board) || tie(board)
+  end
+
+  def player_move(choise, symbol)
     spot = nil
     until spot
       spot = choise.to_i
-      if @board[spot] != "X" && @board[spot] != "O"
-        @board[spot] = @hum
+      if is_position_available?(@board, spot)
+        set_board_position(@board, spot, symbol)
       else
         spot = nil
       end
     end
   end
 
-  def eval_board
-    spot = nil
-    until spot
-      if @board[4] == "4"
-        spot = 4
-        @board[spot] = @com
-      else
-        spot = get_best_move(@board, @com)
-        if @board[spot] != "X" && @board[spot] != "O"
-          @board[spot] = @com
-        else
-          spot = nil
-        end
-      end
-    end
+  def human_choise
+    game_input_output.waiting_for_input
+  end
+
+  def computer_choise(board)
+    return 4 if board[4] == "4"
+    get_best_move(board, @com)
+  end
+
+  def draw_board(draw_board)
+    game_input_output.draw_board(board)
+  end
+
+  def show_input_options
+    game_input_output.show_input_options
+  end
+
+  def show_game_over_message
+    game_input_output.game_over_message
+  end
+
+  def set_board_position(board, spot, mark)
+    board[spot] = mark
+  end
+
+  def is_position_available?(board, spot)
+    board[spot] != "X" && board[spot] != "O"
+  end
+  
+  def keep_playing?
+    !game_is_over(board) && !tie(board)
   end
 
   def get_best_move(board, next_player, depth = 0, best_score = {})
@@ -131,5 +163,5 @@ class Game
 
 end
 
-game = Game.new
-game.start_game
+# game = Game.new
+# game.start_game
--- game_test.rb
+++ game_test.rb
@@ -5,6 +5,21 @@ require 'minitest/autorun'
 require_relative 'game'
 require_relative 'io_test_helpers'
 
+class GameIOSub < GameIO
+  def initialize
+    @content = ''
+  end
+
+  def output(content)
+    @content += content
+    @content
+  end
+
+  def input
+    
+  end
+end
+
 class GameTest < Minitest::Test
   include IoTestHelpers
 
@@ -12,7 +27,7 @@ class GameTest < Minitest::Test
     @subject = Game.new(GameIO.new)
   end
 
-  def test_play_game_with_simple_sequence_0_1_3_to_lose
+  def test_play_game_with_simple_sequence_0_1_3_to_lose_from_default_game_io
     output = " 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \nEnter [0-8]:\n O | 1 | 2 \n===+===+===\n 3 | X | 5 \n===+===+===\n 6 | 7 | 8 \n O | O | X \n===+===+===\n 3 | X | 5 \n===+===+===\n 6 | 7 | 8 \n O | O | X \n===+===+===\n O | X | 5 \n===+===+===\n X | 7 | 8 \nGame over\n"
 
     assert_output output do
@@ -21,13 +36,12 @@ class GameTest < Minitest::Test
   end
 
   ##### Game IO
-
   def test_initial_game_board
     assert_equal @subject.board, ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
   end
 
-  def test_get_human_spot
-    @subject.get_human_spot("0")
+  def test_player_move
+    @subject.player_move("0", "O")
 
     assert_equal @subject.board, ["O", "1", "2", "3", "4", "5", "6", "7", "8"]
   end
@@ -41,5 +55,31 @@ class GameTest < Minitest::Test
     assert_equal game_io.board_template(initial_board), output
   end
 
+  def test_start_game_printing_board
+    initial_board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    output = " 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \nEnter [0-8]:"
+
+    game = Game.new(GameIOSub.new)
+
+    assert_equal game.start_by_printing_the_board(initial_board), output
+  end
+
+  def test_player_move_in_position_0
+    result_board = ["O", "1", "2", "3", "4", "5", "6", "7", "8"]
+
+    @subject.player_move(0, "O")
+
+    assert_equal @subject.board, result_board
+  end
+
+  def test_player_move_in_positions_0_3_7
+    result_board = ["O", "1", "2", "O", "4", "5", "6", "O", "8"]
+    
+    @subject.player_move(0, "O")
+    @subject.player_move(3, "O")
+    @subject.player_move(7, "O")
+
+    assert_equal @subject.board, result_board
+  end
 end
 
