fc661062ac127e9f070abb538c6779e38d66727d
Arthur Granado <agranado@pluga.co>
2017-03-11 11:05:35 -0300

    Why: Handle user bad input or same input.
      * If user write some character that is not 0-8 it have to alert user the is a bad input and ask for right enter.
      * If user choice some position on the board that has been alerady choisen it have to alert user and ask for another enter.

--- game.rb
+++ game.rb
@@ -15,6 +15,14 @@ class GameIO
     output "Game over"
   end
 
+  def bad_input
+    output "Ops... Invalid input!"
+  end
+
+  def position_not_available
+    output "Positions has already been chosen."
+  end
+
   def waiting_for_input
     input
   end
@@ -51,14 +59,22 @@ class Game
     show_input_options
   end
 
+  def show_input_options
+    game_input_output.show_input_options
+  end
+
   def finish_game
     show_game_over_message
+  end
+  
+  def show_game_over_message
+    game_input_output.game_over_message
   end
 
   def play_game(board)
     until did_game_finish?(board)
-      player_move(human_choise, @hum)
-      player_move(computer_choise(board), @com) if keep_playing?
+      player_move(human_choice(board), @hum, board)
+      player_move(computer_choice(board), @com, board) if keep_playing?
       draw_board(board)
     end
   end
@@ -66,38 +82,70 @@ class Game
   def did_game_finish?(board)
     game_is_over(board) || tie(board)
   end
-
-  def player_move(choise, symbol)
-    spot = nil
-    until spot
-      spot = choise.to_i
-      if is_position_available?(@board, spot)
-        set_board_position(@board, spot, symbol)
-      else
-        spot = nil
-      end
-    end
+  
+  def keep_playing?
+    !game_is_over(board) && !tie(board)
   end
-
-  def human_choise
-    game_input_output.waiting_for_input
-  end
-
-  def computer_choise(board)
-    return 4 if board[4] == "4"
-    get_best_move(board, @com)
-  end
-
+  
   def draw_board(draw_board)
     game_input_output.draw_board(board)
   end
 
-  def show_input_options
-    game_input_output.show_input_options
+  def player_move(choice, symbol, board)
+    spot = nil
+    until spot
+      spot = check_and_set_choice_on_the_board(board, choice.to_i, symbol)
+    end
   end
 
-  def show_game_over_message
-    game_input_output.game_over_message
+  def check_and_set_choice_on_the_board(board, spot, symbol)
+    if is_position_available?(@board, spot)
+      set_board_position(@board, spot, symbol)
+    else
+      spot = nil
+    end
+    spot
+  end
+
+  def human_choice(board)
+    input = nil
+    input = game_input_output_human_choice(board) until input
+    input
+  end
+
+  def game_input_output_human_choice(board)
+    input = game_input_output.waiting_for_input
+    input = nil if invalid_input?(input, board)
+    input
+  end
+
+  def invalid_input?(input, board)
+    return true if bad_input?(input, board)
+    return true if position_is_not_available(input, board)
+    false
+  end
+
+  def bad_input?(input, board)
+    if input.match(/[^0-8]/)
+      game_input_output.bad_input 
+      game_input_output.show_input_options
+      return true 
+    end
+    false
+  end
+
+  def position_is_not_available(input, board)
+    if !is_position_available?(board, input.to_i)
+      game_input_output.position_not_available 
+      game_input_output.show_input_options
+      return true
+    end
+    false
+  end
+
+  def computer_choice(board)
+    return 4 if board[4] == "4"
+    get_best_move(board, @com)
   end
 
   def set_board_position(board, spot, mark)
@@ -108,10 +156,6 @@ class Game
     board[spot] != "X" && board[spot] != "O"
   end
   
-  def keep_playing?
-    !game_is_over(board) && !tie(board)
-  end
-
   def get_best_move(board, next_player, depth = 0, best_score = {})
     available_spaces = []
     best_move = nil
--- game_test.rb
+++ game_test.rb
@@ -6,17 +6,19 @@ require_relative 'game'
 require_relative 'io_test_helpers'
 
 class GameIOSub < GameIO
+  attr_accessor :input
+  attr_reader :content
+
   def initialize
     @content = ''
   end
 
-  def output(content)
-    @content += content
-    @content
+  def waiting_for_input
+    input
   end
 
-  def input
-    
+  def output(content)
+    @content += "#{content}"
   end
 end
 
@@ -41,7 +43,7 @@ class GameTest < Minitest::Test
   end
 
   def test_player_move
-    @subject.player_move("0", "O")
+    @subject.player_move("0", "O", @subject.board)
 
     assert_equal @subject.board, ["O", "1", "2", "3", "4", "5", "6", "7", "8"]
   end
@@ -67,7 +69,7 @@ class GameTest < Minitest::Test
   def test_player_move_in_position_0
     result_board = ["O", "1", "2", "3", "4", "5", "6", "7", "8"]
 
-    @subject.player_move(0, "O")
+    @subject.player_move(0, "O", @subject.board)
 
     assert_equal @subject.board, result_board
   end
@@ -75,11 +77,67 @@ class GameTest < Minitest::Test
   def test_player_move_in_positions_0_3_7
     result_board = ["O", "1", "2", "O", "4", "5", "6", "O", "8"]
     
-    @subject.player_move(0, "O")
-    @subject.player_move(3, "O")
-    @subject.player_move(7, "O")
+    @subject.player_move(0, "O", @subject.board)
+    @subject.player_move(3, "O", @subject.board)
+    @subject.player_move(7, "O", @subject.board)
 
     assert_equal @subject.board, result_board
+  end
+
+  def test_handle_player_good_input_number
+    board = ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
+    game_io = GameIOSub.new
+    game_io.input = "1"
+    game = Game.new(game_io)
+
+    assert_equal game.game_input_output_human_choice(board), "1"
+  end
+
+  def test_handle_player_bad_input_not_number_1
+    board = ["0", "O", "2", "3", "X", "5", "6", "7", "8"]
+    game_io = GameIOSub.new
+    game_io.input = "x"
+    game = Game.new(game_io)
+
+    game.game_input_output_human_choice(board)
+
+    assert_equal game_io.content, "Ops... Invalid input!Enter [0-8]:"
+  end
+
+  def test_handle_player_bad_input_not_number_2
+    board = ["X", "O", "2", "3", "X", "5", "6", "O", "8"]
+    game_io = GameIOSub.new
+    game_io.input = "x"
+    game = Game.new(game_io)
+
+    game.game_input_output_human_choice(board)
+
+    assert_equal game_io.content, "Ops... Invalid input!Enter [0-8]:"
+  end
+
+  def test_handle_player_bad_input_position_has_already_been_chosen
+    board = ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
+    game_io = GameIOSub.new
+    game_io.input = "4"
+    game = Game.new(game_io)
+
+    game.game_input_output_human_choice(board)
+
+    assert_equal game_io.content, "Positions has already been chosen.Enter [0-8]:"
+  end
+
+  def test_despite_of_player_bad_input_should_keep_ask_for_correct_input
+    board = ["0", "1", "2", "3", "X", "5", "6", "7", "8"]
+    game_io = GameIOSub.new
+    game_io.input = "4"
+    game = Game.new(game_io)
+
+    game.game_input_output_human_choice(board)
+    game_io.input = "5"
+    game.game_input_output_human_choice(board)
+
+    assert_equal game.game_input_output_human_choice(board), "5"
+    assert_equal game_io.content, "Positions has already been chosen.Enter [0-8]:"
   end
 end
 
